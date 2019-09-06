35b934e110a25be8c03e11b33bb5b1f7dd7d1753
zhangchen <zc_program_private@163.com>
2019-09-06 23:17:03 +0800

    添加图片详情查看相关功能

--- app/src/main/java/me/everzc/webviewimage/MainActivity.java
+++ app/src/main/java/me/everzc/webviewimage/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Html5WebView mWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebView=findViewById(R.id.web_view);
     }
 }
